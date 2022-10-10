789979f2726465ee62a1c2984fb0a42c83e30683
mustofa855 <203040030@mail.unpas.ac.id>
2022-10-10 13:40:10 +0700

    Menyelesaikan Tugas 3 Video 6 - Assets

---
+++ phpmvc/app/core/Constants.php
@@ -0,0 +1,3 @@
+<?php 
+
+define('BASEURL','http://localhost/prakweb_A_203040030/phpmvc/public/');
--- phpmvc/app/views/about/index.php
+++ phpmvc/app/views/about/index.php
@@ -1,4 +1,6 @@
-
-    <h1>About Me</h1>
+<div class="container">
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASEURL; ?>/img/profile.jpg" width=250 height=250 alt="Mustofa Firdaus" class="rounded-circle shadow">
     <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, Saya adalah seorang <?= 
     $data['pekerjaan']; ?>.</p>
+</div>
